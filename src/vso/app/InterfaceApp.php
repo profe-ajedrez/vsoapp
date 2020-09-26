7b1f43a93d7327456efeef06b1b2cec067a88374
profe-ajedrez <chess.coach.ar@gmail.com>
2020-09-25 23:13:37 -0300

    removed method getProperty from InterfaceApp

--- src/vso/app/InterfaceApp.php
+++ src/vso/app/InterfaceApp.php
@@ -11,15 +11,4 @@ interface InterfaceApp
     public function getLogger() : logger;
     public function getWhooper() : Run;
     public function getRouter() : InterfaceRouter;
-
-    /**
-     * getProperty
-     *
-     * Implementing classes should use this method stub to expose
-     * needed properties to parent class
-     *
-     * @param string $property
-     * @return mixed
-     */
-    public function getProperty(string $property);
 }
