13f4c2371fad1e50501a2b929ddca86e123dd4ae
profe-ajedrez <chess.coach.ar@gmail.com>
2020-09-25 23:57:08 -0300

    reworked InterfaceApp.php

--- src/vso/app/InterfaceApp.php
+++ src/vso/app/InterfaceApp.php
@@ -5,10 +5,16 @@ namespace vso\app;
 use \Monolog\Logger;
 use \Whoops\Run;
 use \vso\http\router\InterfaceRouter;
+use \vso\mailcontainer\InterfaceMailContainer;
 
 interface InterfaceApp
 {
     public function getLogger() : logger;
     public function getWhooper() : Run;
     public function getRouter() : InterfaceRouter;
+    public function getMailContainer() : InterfaceMailContainer;
+    public function setLogger(logger $logger) : void;
+    public function setWhooper(Run $whooper) : void;
+    public function setRouter(InterfaceRouter $router) : void;
+    public function setMailContainer(InterfaceMailContainer $mailContainer) : void;
 }
